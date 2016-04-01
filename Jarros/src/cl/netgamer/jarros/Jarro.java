faa731825e6972d09fca39b45534da6af0272499
atesin <atesin@TITAN>
2016-03-31 21:43:32 -0300

    fix jarro

--- Jarros/src/cl/netgamer/jarros/Jarro.java
+++ Jarros/src/cl/netgamer/jarros/Jarro.java
@@ -2,10 +2,49 @@ package cl.netgamer.jarros;
 
 public class Jarro
 {
-
-	public Jarro()
+	private int capacidad;
+	private int contenido;
+	
+	public Jarro(int capacidad)
 	{
-		// TODO Auto-generated constructor stub
+		this.capacidad = capacidad;
 	}
-
+	
+	int getCapacidad()
+	{
+		return capacidad;
+	}
+	
+	int getContenido()
+	{
+		return contenido;
+	}
+	
+	int getDisponible()
+	{
+		return capacidad - contenido;
+	}
+	
+	void vaciar()
+	{
+		contenido = 0;
+	}
+	
+	void llenar()
+	{
+		contenido = capacidad;
+	}
+	
+	void llenar(int relleno)
+	{
+		contenido = Math.min(contenido + relleno, capacidad);
+	}
+	
+	void trasVaciar(Jarro destino)
+	{
+		int relleno = Math.min(destino.getDisponible(), contenido);
+		destino.llenar(relleno);
+		contenido -= relleno;
+	}
+	
 }
